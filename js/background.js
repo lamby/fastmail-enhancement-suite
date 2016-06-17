31bfae08727cbbc1d39892316b88e99f2b0533b5
Chris Lamb <chris@chris-lamb.co.uk>
2016-06-17 20:13:30 +0100

    Use single quotes for computer stuff
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- js/background.js
+++ js/background.js
@@ -5,13 +5,13 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 });
 
 chrome.contextMenus.create({
-  "title": "Append '%s' to subject",
-  "contexts": ["selection"],
-  "documentUrlPatterns": [
-      "http://*.fastmail.com/mail/compose*"
-    , "https://*.fastmail.com/mail/compose*"
+  'title': "Append '%s' to subject",
+  'contexts': ['selection'],
+  'documentUrlPatterns': [
+      'http://*.fastmail.com/mail/compose*'
+    , 'https://*.fastmail.com/mail/compose*'
   ],
-  "onclick": function(info, tab) {
+  'onclick': function(info, tab) {
     chrome.tabs.sendMessage(tab.id, {
         'cmd': 'appendToSubject'
       , 'selectionText': info.selectionText
