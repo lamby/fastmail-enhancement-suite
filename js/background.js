03b88b43271a85812a6aae2a9ae21759cd3c9aba
Chris Lamb <chris@chris-lamb.co.uk>
2016-06-17 19:59:02 +0100

    diff friendly.

--- js/background.js
+++ js/background.js
@@ -8,13 +8,13 @@ chrome.contextMenus.create({
   "title": "Append '%s' to subject",
   "contexts": ["selection"],
   "documentUrlPatterns": [
-    "http://*.fastmail.com/mail/compose*",
-    "https://*.fastmail.com/mail/compose*"
+      "http://*.fastmail.com/mail/compose*"
+    , "https://*.fastmail.com/mail/compose*"
   ],
   "onclick": function(info, tab) {
     chrome.tabs.sendMessage(tab.id, {
-      'cmd': 'appendToSubject',
-      'selectionText': info.selectionText
+      , 'cmd': 'appendToSubject'
+        'selectionText': info.selectionText
     });
   }
 });
