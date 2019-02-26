lamby/fastmail-enhancement-suite

commit e8253b2e9a24c4a4cb0b61e41bb1dabab052947f

Rewrite setSubject as a get and a set.

js/main.js
@@ -82,6 +82,21 @@ function FastMailEnhancementSuite(options) {
         clearTimeout(timeoutID);
         fn(eval(val));
       }, interval);
+    },
+    getSubject: function () {
+      var instance = FastMail.mail.screens.compose.instance;
+
+      return instance.subject;
+    },
+    setSubject: function (subject) {
+      var instance = FastMail.mail.screens.compose.instance;
+
+      // Set the underlying data store; simply adjusting the element won't change
+      // the email once sent.
+      instance.subject = subject;
+
+      // Update the "Subject" HTML element too
+      $('.s-compose-subject input').val(subject);
     }
   });
 
@@ -160,13 +175,8 @@ function FastMailEnhancementSuite(options) {
   });
 
   this.appendToSubject = function (request) {
-    var instance = FastMail.mail.screens.compose.instance;
-
-    // Set the underlying data store; simply adjusting the element won't change
-    // the email once sent.
-    instance.subject += request.selectionText.replace(/\s+/g, ' ');
-
-    // Update the "Subject" element
-    $('.s-compose-subject input').val(instance.subject);
+    this.setSubject(
+      $.getSubject() + request.selectionText.replace(/\s+/g, ' ')
+    );
   };
 }

commit 7aaa3ef869b1136e0f0384faf590f708a3f8380c

Fix broken right-click "append to subject".
Was broken since e8253b2e9a24c4a4cb0b61e41bb1dabab052947f, alas.

js/main.js
@@ -244,7 +244,7 @@ function FastMailEnhancementSuite(options) {
   });
 
   this.appendToSubject = function (request) {
-    this.setSubject(
+    $.setSubject(
       $.getSubject() + request.selectionText.replace(/\s+/g, ' ')
     );
   };